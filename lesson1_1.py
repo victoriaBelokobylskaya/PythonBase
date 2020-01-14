1f8faa222cc4646999921f205b73a6f5a914166f
victoriaBelokobylskaya <infvitta@gmail.com>
2020-01-15 00:00:46 +0300

    Первое задание

---
+++ lesson1_1.py
@@ -0,0 +1,15 @@
+line = 'First line'
+print(line)
+
+i_number = 99
+print(i_number)
+
+f_number = 68.56
+print(f_number)
+
+user = input('Введите логин: ')
+password = input('Введите пароль: ')
+print(user, password)
+
+user_num = input('Введите число от 1 до 100: ')
+print(user_num)
