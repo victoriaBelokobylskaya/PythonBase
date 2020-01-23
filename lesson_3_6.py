730ff0298fac07d2211f0e19e18791b8b3acc845
victoriaBelokobylskaya <infvitta@gmail.com>
2020-01-23 22:05:19 +0300

    Урок 3 задание 6

--- lesson_3_6.py
+++ lesson_3_6.py
@@ -1,13 +1,13 @@
-def int_func(text):
-    return str(text).title()
-
+# def int_func(text):
+#     return str(text).title()
 
 long_str = input('Введите строку, разделенную пробелами: ').split(' ')
 
 new_str = ''
+word = lambda text: str(text).title()
 
 for i in long_str:
-    word = int_func(i)
-    new_str += str(word) + ' '
+    # word = int_func(i)
+    new_str += str(word(i)) + ' ' # переделала на lambda функцию
 print(new_str)
 
